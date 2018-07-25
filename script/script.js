61fa2985a8240857dc756f26cf9c8e075e92b362
marjorii <hello@marjorieober.com>
2018-07-25 21:28:03 +0200

    Add load prototype to Img

--- script/script.js
+++ script/script.js
@@ -3,31 +3,51 @@
 /* load JSON */
 
 function readJSONFile(url) {
-    return new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest();
-        request.overrideMimeType("application/json");
-        request.open("GET", url);
-        request.onload = () => {
-            if (request.readyState === 4 && request.status == "200") {
-                resolve(JSON.parse(request.responseText));
-            }
-        }
-        request.onerror = () => reject(Error("Couldn't load " + url));
-        request.send();
-    });
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.overrideMimeType("application/json");
+    request.open("GET", url);
+    request.onload = () => {
+      if (request.readyState === 4 && request.status == "200") {
+        resolve(JSON.parse(request.responseText));
+      }
+    }
+    request.onerror = () => reject(Error("Couldn't load " + url));
+    request.send();
+  });
 }
 
 
 //OBJECTS
-function Img(name) {
 
+function Img(options) {
+  this.title = options.title;
+  this.elem = undefined;
+  this.uri = "medias/img/medium/1-norvege/" + this.title;
 }
+Img.prototype.load = function() {
+  return new Promise ((resolve, reject) => {
+    this.elem = new Image();
+    this.elem.onload = () => {
+      resolve();
+    };
+    this.elem.onerror = () => {
+      reject(new Error("Couldn't find" + this.uri));
+    };
+    this.elem.src = this.uri;
+  });
+};
+
 
 
 //CORE
+
 async function init() {
   var json = await readJSONFile("script/sources.json");
-  console.log(json);
+  var jsonImg = json.chapters[0].subChapters[0].medias[0];
+  var img = new Img(jsonImg);
+
+  await img.load();
 }
 
 init();
